80e5e9c9ae60d33de8123cddb0804289f64ef466
Abdulrahman <bakareabdulrahman001@gmail.com>
2024-11-18 20:28:57 +0000

    tftu

---
+++ src/pages/Nurse/Nurse.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './NurseDashboard.css';
+
+const NurseDashboard = () => {
+  return (
+    <div className="nurse-dashboard">
+      <h2>Nurse Dashboard</h2>
+      <p>Welcome to your dashboard. Here you can update patient status, assist with scheduling, and more.</p>
+    </div>
+  );
+};
+
+export default NurseDashboard;
---
+++ src/pages/doctor/DoctorDashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DoctorDashboard.css';
+
+const DoctorDashboard = () => {
+  return (
+    <div className="doctor-dashboard">
+      <h2>Doctor Dashboard</h2>
+      <p>Welcome to your dashboard. Here you can manage your appointments, view patient records, and more.</p>
+    </div>
+  );
+};
+
+export default DoctorDashboard;
