e1894dc3aff3550be1db838e7c9daabb88810b17
Abdulrahman <bakareabdulrahman001@gmail.com>
2024-11-18 17:10:30 +0000

    Addedgit push origin main

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+// src/Pages/Login.js
+import React from 'react';
+import LoginForm from '../components/LoginForm';
+
+const Login = () => {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
