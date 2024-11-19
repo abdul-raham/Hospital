a561351076c1e9c13b432222d9389d9cf1070634
abdulsamad <olanrewajuabdulsamad6@gmail.com>
2024-11-19 09:00:21 +0100

    Added root doc

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import NurseDashboard from "./pages/Nurse/NurseDashboard";
+import PatientDashboard from "./pages/Patient/PatientDashboard";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+
+        {/* Doctor Role */}
+        <Route path="/doctor" element={<DoctorDashboard />} />
+
+        {/* Nurse Role */}
+        <Route path="/nurse" element={<NurseDashboard />} />
+
+        {/* Patient Role */}
+        <Route path="/patient" element={<PatientDashboard />} />
+
+        {/* Catch-all for 404 */}
+        <Route
+          path="*"
+          element={
+            <div style={{ textAlign: "center", marginTop: "20%" }}>
+              <h1>404 - Page Not Found</h1>
+              <p>The page you're looking for doesn't exist.</p>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
