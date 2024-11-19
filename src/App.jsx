0e2bd888131e066e3e27a0ced47f57e653679859
Abdulrahman <bakareabdulrahman001@gmail.com>
2024-11-19 11:12:32 +0000

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login/Loginpage";
+import Login from "./pages/Login/LoginPage";
 import DoctorDashboard from "./Roles/doctor/DoctorDashboard";
 import NurseDashboard from "./Roles/Nurse/NurseDashboard";
 import PatientDashboard from "./Roles/Patient/PatientDashboard";
