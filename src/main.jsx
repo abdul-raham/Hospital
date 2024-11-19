3279100309b0856873088ea6afc1e8525dbf9f22
abdulsamad <olanrewajuabdulsamad6@gmail.com>
2024-11-19 16:19:28 +0100

    update App.jsx, DoctorDashboard.jsx and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Assuming you have styles
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
