59c4b65b7681787608db6011484116cca0dbf220
abdulsamad <olanrewajuabdulsamad6@gmail.com>
2024-11-19 13:17:31 +0100

    edited firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,12 @@
-// src/auth.js
 import { getAuth, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';  // Make sure to import getFirestore
 
 const firebaseConfig = {
   apiKey: "AIzaSyCdmHvjKgjmPPqJgmlIk2vYMXjdwcpf7hA",
   authDomain: "hosp-429ad.firebaseapp.com",
   projectId: "hosp-429ad",
-  storageBucket: "hosp-429ad.appspot.com",  // corrected bucket URL
+  storageBucket: "hosp-429ad.appspot.com",
   messagingSenderId: "420255599423",
   appId: "1:420255599423:web:a476f91fcaa0f49121218a",
   measurementId: "G-6Y7Q7W5MWR"
@@ -14,8 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); // Initialize Firebase Auth instance
-// Initialize Firestore
-export const db = getFirestore(app);
+const auth = getAuth(app);  // Initialize Firebase Auth instance
+const db = getFirestore(app);  // Initialize Firestore
 
-export { auth, createUserWithEmailAndPassword, signOut, db };
+export { auth, createUserWithEmailAndPassword, signOut, db };  // Export all at once
